78cb745f235542f14653b235b450e3c7fc4afd75
jsrkamal2002 <veerammarish89@gmail.com>
2022-09-29 07:41:06 +0530

    CAT-1

--- CAT-1/MAD/con1.py
+++ CAT-1/MAD/con1.py
@@ -3,7 +3,7 @@ from pdf2image import convert_from_path
 
 
 # Store Pdf with convert_from_path function
-images = convert_from_path('3 bigqut.pdf')
+images = convert_from_path('single code.pdf')
 # print(images)
 print("started!!!")
 
