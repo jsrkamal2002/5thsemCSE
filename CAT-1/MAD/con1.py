4931095f663b9268340f9c7ac1cf457e6f197d43
jsrkamal <90234628+jsrkamal2002@users.noreply.github.com>
2022-09-29 07:14:16 +0530

    CAT-1

--- CAT-1/MAD/con1.py
+++ CAT-1/MAD/con1.py
@@ -4,7 +4,7 @@ from pdf2image import convert_from_path
 
 # Store Pdf with convert_from_path function
 print("started!!!")
-images = convert_from_path('Android architecture.pdf')
+images = convert_from_path('2mark.pdf')
 print(images)
 
 for i in range(len(images)):
