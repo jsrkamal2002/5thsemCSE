6a061f67e51027968b330c824bb73ab4ad9f1137
jsrkamal <90234628+jsrkamal2002@users.noreply.github.com>
2022-10-06 08:10:17 +0530

    CAT-1

--- CAT-1/IPR/con1.py
+++ CAT-1/IPR/con1.py
@@ -3,13 +3,15 @@ from pdf2image import convert_from_path
 
 
 # Store Pdf with convert_from_path function
-images = convert_from_path('intellectual-property-rights BOOK PANDEY.pdf')
+images = convert_from_path('PDF Gallery_20221005_233628.pdf')
 # print(images)
 print("started!!!")
 
 for i in range(len(images)):
 
 	# Save pages as images in the pdf
+	# print the number of times the loop is running
+	print(i)
 	images[i].save('page'+ str(i) +'.jpg', 'JPEG')
 
 print("Done")
