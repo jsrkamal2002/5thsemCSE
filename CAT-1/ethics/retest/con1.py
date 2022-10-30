6857f8ce8a04971a0ecc232212fd95e8827f6790
jsrkamal <90234628+jsrkamal2002@users.noreply.github.com>
2022-10-30 21:45:06 +0530

    CAT-1

---
+++ CAT-1/ethics/retest/con1.py
@@ -0,0 +1,16 @@
+# import module
+from pdf2image import convert_from_path
+
+
+# Store Pdf with convert_from_path function
+images = convert_from_path('ethics 2 mark .pdf')
+
+print("Start")
+
+for i in range(len(images)):
+
+	# Save pages as images in the pdf
+	images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+	print("page"+ str(i) +".jpg")
+ 
+print("Done")
