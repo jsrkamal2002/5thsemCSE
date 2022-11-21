5799efba0e456e27877d1124fce0817e389af6d7
jsrkamal2002 <veerammarish89@gmail.com>
2022-11-21 21:03:34 +0530

    CAT-2

--- CAT-2/CN/RAW/con1.py
+++ CAT-2/CN/RAW/con1.py
@@ -3,7 +3,7 @@ from pdf2image import convert_from_path
 
 
 # Store Pdf with convert_from_path function
-images = convert_from_path('UNIT III CN (1).pdf')
+images = convert_from_path('World Wide Web and HTTP - FTP - Electronic Mail - Telnet - Secure Shell - Domain Name System.pdf')
 print("start")
 
 for i in range(len(images)):
