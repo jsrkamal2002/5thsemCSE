e89100cbd8f8823f5efac2261eae19cfca385eca
jsrkamal2002 <veerammarish89@gmail.com>
2022-11-24 05:43:51 +0530

    CAT-2

---
+++ CAT-2/MAD/RAW/con1.py
@@ -0,0 +1,14 @@
+# import module
+from pdf2image import convert_from_path
+
+
+# Store Pdf with convert_from_path function
+images = convert_from_path('View and View Layout.pdf')
+print("start")
+
+for i in range(len(images)):
+
+	# Save pages as images in the pdf
+	images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+
+print("Done")
