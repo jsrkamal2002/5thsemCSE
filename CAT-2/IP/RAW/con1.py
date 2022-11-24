dbb50eede51e180d204e4cb2398f5a1904643cdd
jsrkamal <90234628+jsrkamal2002@users.noreply.github.com>
2022-11-24 14:55:07 +0530

    CAT-2

---
+++ CAT-2/IP/RAW/con1.py
@@ -0,0 +1,15 @@
+# import module
+from pdf2image import convert_from_path
+
+
+# Store Pdf with convert_from_path function
+print("started!!!")
+images = convert_from_path('php_prog.pdf')
+print(images)
+
+for i in range(len(images)):
+
+	# Save pages as images in the pdf
+	images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+
+print("Done")
