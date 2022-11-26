54ad8c1a269b1b506eb1e52affb222f65ae68bb2
jsrkamal <90234628+jsrkamal2002@users.noreply.github.com>
2022-11-26 19:58:04 +0530

    SEM-LAB

---
+++ lab/con1.py
@@ -0,0 +1,15 @@
+# import module
+from pdf2image import convert_from_path
+
+
+# Store Pdf with convert_from_path function
+print("started!!!")
+images = convert_from_path('FXEC_III_NW_Lab_Manual_final.pdf')
+print(images)
+
+for i in range(len(images)):
+
+	# Save pages as images in the pdf
+	images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+
+print("Done")
