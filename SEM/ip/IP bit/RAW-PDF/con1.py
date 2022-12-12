7c09472b2189e1237eb804dea9662bf479eab59d
jsrkamal2002 <veerammarish89@gmail.com>
2022-12-12 08:19:03 +0530

    SEM

--- SEM/ip/IP bit/RAW-PDF/con1.py
+++ SEM/ip/IP bit/RAW-PDF/con1.py
@@ -4,7 +4,7 @@ from pdf2image import convert_from_path
 
 # Store Pdf with convert_from_path function
 print("started!!!")
-images = convert_from_path('XML XSLT.pdf')
+images = convert_from_path('HTML Tables.pdf')
 print(images)
 
 for i in range(len(images)):
