1e3ec86bec4708379f766febb7d67012cd795a8b
jsrkamal <90234628+jsrkamal2002@users.noreply.github.com>
2022-12-14 13:13:11 +0530

    SEM

--- SEM/MAD/PDF/con1.py
+++ SEM/MAD/PDF/con1.py
@@ -4,7 +4,7 @@ from pdf2image import convert_from_path
 
 # Store Pdf with convert_from_path function
 print("started!!!")
-images = convert_from_path('mobile vs web app.pdf')
+images = convert_from_path('ios hello world.pdf')
 print(images)
 
 for i in range(len(images)):
